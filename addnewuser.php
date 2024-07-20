76364b784ba672177ca6ff825bad05d79481a685
morganweltzer <weltzeme@mail.uc.edu>
2024-07-19 22:48:38 -0400

    updating table

--- addnewuser.php
+++ addnewuser.php
@@ -5,9 +5,9 @@
 	$lastname = $_POST["lastname"];
 	$email = $_POST["email"];
 	$phone = $_POST["phone"];
-	if(isset($username) and isset ($password)){
+	if(isset($username) and isset ($password) and isset($firstname) and isset($lastname) and isset($email) and isset($phone)){
 		//echo "Debug> got username=$username;password=$password";
-		if (addnewuser($username,$password)) {
+		if (addnewuser($username, $password, $firstname, $lastname, $email, $phone)) {
 			echo "Registration Sucessful";
 		}else{
 			echo "Registration failed";
@@ -17,15 +17,15 @@
 		echo "No username or password set";
 	}
 
-  	function addnewuser($username, $password) {
+  	function addnewuser($username, $password, $firstname, $lastname, $email, $phone) {
   		$mysqli = new mysqli('localhost', 'weltzeme', '1234', 'waph');
   		if($mysqli->connect_errno){
   			printf("Database connection failed: %s\n", $mysqli->connect_error);
   			return FALSE;
   		}
-	  	$prepared_sql = "INSERT INTO users (username,password) VALUES (?,md5(?))";
+	  	$prepared_sql = "INSERT INTO users (username, password, firstname, lastname, email, phone) VALUES (?, md5(?), ?, ?, ?, ?)";
 	  	$stmt = $mysqli->prepare($prepared_sql);
-	  	$stmt->bind_param("ss", $username,$password);
+	  	$stmt->bind_param("ss", $username, $password, $firstname, $lastname, $email, $phone);
 	  	if($stmt->execute()) return TRUE;
 	  	return FALSE;
   	}
